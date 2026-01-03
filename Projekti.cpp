3f99c56a7eec54d7a6be7bc1c507d75b5d72ef15
RedonPajaziti <113989993+RedonPajaziti@users.noreply.github.com>
2026-01-03 01:27:21 +0100

    Creating the function "vendosKohen" (which takes values from the keyboard)

--- Projekti.cpp
+++ Projekti.cpp
@@ -12,6 +12,26 @@ struct Ora{
     void shfaqOren();
 };
 
+void Ora::vendosKohen(){
+    cout<< "Sheno oren: ";
+    cin >> ora;
+    if(ora < 0 || ora > 23){
+        ora=0;
+    }
+
+    cout << "Sheno minutat: ";
+    cin >> minuta;
+    if(minuta < 0 || minuta > 60){
+        minuta=0;
+    }
+
+    cout << "Sheno sekondat: ";
+    cin >> sekonda;
+    if(sekonda < 0 || sekonda > 60){
+        sekonda=0;
+    }
+};
+
 
 int main(){
 
