275838d3fd377909be1ccbfa637fbb7239c9b2e0
RedonPajaziti <113989993+RedonPajaziti@users.noreply.github.com>
2026-01-03 22:10:15 +0100

    Adding the function "shfaqOren()" which is used to cout our time in the end.

--- Projekti.cpp
+++ Projekti.cpp
@@ -69,9 +69,20 @@ void Ora::shtoOre(){
         }
 };
 
+void Ora::shfaqOren(){
+    cout<<"Ora eshte: "<< ora << ":" << minuta << ":" << sekonda << endl;
+};
 
 
 int main(){
+    Ora O1;
+    O1.vendosKohen();
+
+    char shkronja;
+
+    cout<< "Sheno me S per te shtuar nje sekond, me M per te shtuar nje minute, me O per nje ore." << endl;
+    cout << "Sheno me X nese nuk deshiron te shtosh diqka" << endl;
+
 
     return 0;
 }
